646748fc97f71c1981168209e47899c6d12a8a6c
Daniel Nava <dan3.nm@gmail.com>
2023-03-23 17:00:55 -0600

    FIRST COMMIT: app deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Heroku deploy
         </p>
         <a
           className="App-link"
